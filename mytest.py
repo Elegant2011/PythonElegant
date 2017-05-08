4515ff3a1818e71213be26fb6171364ae131cf3a
DESKTOP-EBNIN6J\Dell <starelegant@163.com>
2017-05-08 13:30:21 +0800

    edit my test .py

---
+++ mytest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/2/22 20:38
+# @Author  : Elegant
+# @Site    :
+# @File    : mytest.py 
+# @Software: PyCharm Community Edition
+
+print(123)
