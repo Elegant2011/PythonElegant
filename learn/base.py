3a0d4528f9c863dbea1809309228180c1ba03f01
zhangmin <min.zhang@ucloud.cn>
2020-01-17 18:31:03 +0800

    add base lean

---
+++ learn/base.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#print('\\\t\\')
+print('ABC'.encode('ascii'))
+print('中文'.encode('utf-8'))
+print(u'中文测试')
+
+userID = 'hello,%s' % 'test'
+print(userID)
+
+userName ='hello,{1},{0}'.format('pass',int(77.77))
+print(userName)
+
+print ("----------------")
+
+# s = input('birth:')
+# birth = int(s)
+# if birth < 2000:
+#     print('00前')
+# else:
+#     print('00后')
+
+names =['Michael','Bob','Tracy']
+for name in names:
+    print(name)
+print ("---------------- \n")
+
+L = ['Bart', 'Lisa', 'Adam']
+n =0
+while n <len(L):
+    print(L[n])
+    n = n + 1
+
