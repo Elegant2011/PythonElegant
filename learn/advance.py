e0f6ed9c29068223d6c26c209dedb26552a3ba71
zhangmin <min.zhang@ucloud.cn>
2020-01-19 18:16:19 +0800

    add list expression

--- learn/advance.py
+++ learn/advance.py
@@ -1,4 +1,4 @@
-from collections import Iterable
+#from collections import Iterable
 
 L = ['Michael','Sarah','Tracy','Bob','Jack']
 print (L[0:3])
@@ -16,11 +16,43 @@ def trim(s):
     print('len ',len(s),'after:',s)
     return s
 
-
-trim('hello  ')
+#trim('hello  ')
 trim('       hello  pass ')
 
 # 2、 迭代
 d = {'a': 1, 'b': 2, 'c': 3}
 for key in d:
     print(key)
+
+#print (isinstance('abc', Iterable) ) # str是否可迭代
+
+def findMinAndMax(L):
+     if len(L)==0:
+         return (None, None)
+     min,max =L[0],L[0]
+     for k in L:
+      if k >= max:
+        max = k
+      if k <=min:
+        min = k
+     return (min,max)
+
+
+if findMinAndMax([]) != (None, None):
+    print('测试失败! 1')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败! 2')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败! 3')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败! 4')
+else:
+    print('测试成功! 5')
+
+
+print (list(range(1,11)))
+print([x * x for x in range(1,11)])
+
+print([x * x for x in range(1,11)if x %2 == 0])
+
+print([m + n for m in 'ABC' for n in 'XYZ'])
